d9cdc095d4aae173845b43143a8d9283b561d7cc
Jiri Vyoralek <jiri@vyoralek.cz>
2022-05-08 18:30:45 +0200

    ESPHome / Helper - script for weather icons list

---
+++ esphome/helpers/weather-icons.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+import json
+import re
+
+url = urlopen("https://raw.githubusercontent.com/Templarian/MaterialDesign/master/meta.json")
+meta = [(i['name'], i['codepoint']) for i in json.loads(url.read()) if re.search('^weather-', i['name'])]
+
+print('''---
+esphome:
+  # ...
+  includes:
+    - weather_icon_map.h
+
+# ...
+
+font:
+  - file: fonts/materialdesignicons-webfont.ttf
+    id: ...
+    size: ...
+    glyphs:''')
+
+for name, codepoint in meta:
+    print('      - "\\U000%s" # %s' % (codepoint, name))
+
+with open('weather_icon_map.h', 'w') as h:
+    h.write('#include <map>\nstd::map<std::string, std::string> weather_icon_map\n')
+    h.write('  {\n')
+    for name, codepoint in meta:
+        h.write('    {"%s", "\\U000%s"},\n' % (name.replace('weather-', ''), codepoint))
+    h.write('  };')
